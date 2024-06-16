d1ac6f9678a5db42ab6587a01b3b987faaf07ccb
Ahmed Mohammed Wahba <67748256+abowahbaz@users.noreply.github.com>
2024-06-16 09:48:48 +0300

    added meta.js

---
+++ atcoder-dark-theme.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         AtCoder Theme: Refined Dark
+// @namespace    http://atcoder.jp/
+// @version      0.2.3
+// @description  Refined dark theme for AtCoder
+// @author       magurofly , abowahbaz
+// @match        https://atcoder.jp/*
+// @grant        none
+// ==/UserScript==
